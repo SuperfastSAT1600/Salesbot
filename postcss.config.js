b8bb9fd25b7d343c5895fc5f002fd995434e8d5d
superfastsat <superfastsat@naver.com>
2025-06-05 13:37:50 +0900

    Update PostCSS config to array format

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+// 파일 경로: postcss.config.js (프로젝트 루트에 위치)
+module.exports = {
+  plugins: [
+    'tailwindcss',
+    'autoprefixer',
+    // 다른 PostCSS 플러그인을 사용하신다면 여기에 문자열 형태로 추가할 수 있습니다.
+  ],
+};
